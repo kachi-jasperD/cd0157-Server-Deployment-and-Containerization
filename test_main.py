cebb6200b420cd6325a6e6111a2325f69dfca3e7
Onyekachi Jasper-Duruzor <onyekachijasper-duruzor@Onyekachis-MacBook-Pro.local>
2022-08-04 05:06:24 +0100

    invalid test/False

--- test_main.py
+++ test_main.py
@@ -15,7 +15,7 @@ PASSWORD = 'huff-puff'
 @pytest.fixture
 def client():
     os.environ['JWT_SECRET'] = SECRET
-    main.APP.config['TESTING'] = False
+    main.APP.config['TESTING'] = True
     client = main.APP.test_client()
 
     yield client
@@ -26,6 +26,7 @@ def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == 'Healthy'
+    assert response.json == False
 
 
 def test_auth(client):
